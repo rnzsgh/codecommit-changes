0616fd79f5de4aa90747c2e0af251c9b5fdf5ec2
Ryan <ryannitz@amazon.com>
2018-09-17 17:50:04 -0400

    Add throttle checks

--- lambda_function.py
+++ lambda_function.py
@@ -2,14 +2,17 @@
 import json
 import boto3
 import os
+import time
+import sys
 from botocore.exceptions import ClientError
-
 
 topic = os.environ['OUTPUT_TOPIC_ARN']
 codecommit = boto3.client('codecommit')
 sns = boto3.client('sns')
 
 MAX_COMMITS = 20
+
+sys.setrecursionlimit(5000)
 
 def handler(event, context):
     """Handler function for the Lambda"""
@@ -34,29 +37,37 @@ def handler(event, context):
 
 def commits(repo, region, before, payload, commit_id):
     """Recursively walk through the commits"""
-
     if commit_id == before:
         return
 
-    commit = codecommit.get_commit(repositoryName=repo, commitId=commit_id)
+    commit = None
+    while True:
+        try:
+            commit = codecommit.get_commit(repositoryName=repo, commitId=commit_id)
+            break
+        except ClientError as err:
+            if err.response['Error']['Code'] != 'ThrottlingException':
+                raise err
+            time.sleep(1)
 
     commit.pop('ResponseMetadata', None)
     commit['commit']['link'] = 'https://console.aws.amazon.com/codecommit/home?region=%s#/repository/%s/commit/%s' % (region, repo, commit_id)
 
     append(payload, commit)
 
-    if 'parents' not in commit['commit'] or commit['commit']['parents'] is None or len(commit['commit']['parents']) is 0:
-        return
+    if 'parents' in commit['commit'] and commit['commit']['parents'] is not None:
+        for parent in commit['commit']['parents']:
+            if parent == before:
+                return
+            commits(repo, region, before, payload, parent)
 
-    for parent in commit['commit']['parents']:
-        if parent == before:
-            return
-        commits(repo, region, before, payload, parent)
+
+
 
 
 def append(payload, commit):
     """Append up the max commits and increase the size/count"""
-    payload['size'] = payload['size'] + 1
+    payload['size'] += 1
 
     if len(payload['commits']) <= MAX_COMMITS:
         payload['commits'].append(commit['commit'])
