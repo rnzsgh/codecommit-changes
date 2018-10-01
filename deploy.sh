18aef234799c210a7ea09b678bb11d2802f4527a
Ryan <ryannitz@amazon.com>
2018-10-01 09:43:21 -0400

    Change name

--- deploy.sh
+++ deploy.sh
@@ -2,7 +2,7 @@
 
 python -m py_compile lambda_function.py
 
-zip -r handler.zip lambda_function.py ; aws lambda update-function-code --function-name TestCodeCommitPython --publish --zip-file fileb://handler.zip
+zip -r handler.zip lambda_function.py ; aws lambda update-function-code --function-name CodeCommitChanges --publish --zip-file fileb://handler.zip
 
 
 
